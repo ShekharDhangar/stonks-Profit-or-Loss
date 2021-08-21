18c506c9eb76b3d6601be4c97b72768c8830b977
Shekhar Dhangar <shekhardhangar@yahoo.com>
2021-08-21 18:06:12 +0530

    Updated Index JS
    Changed formula

--- index.js
+++ index.js
@@ -5,33 +5,31 @@ const calculateBtn = document.querySelector(".calculate-btn");
 const output = document.querySelector(".output");
 
 function calculateProfitandLoss(buyPrice,sellPrice,numOfStock){
-
     if(sellPrice>buyPrice){
-        const profit = sellPrice-buyPrice;
-        const profitPercentage = ((profit/buyPrice)*100).toFixed(2);
-        // return ()
+        const profit = (sellPrice-buyPrice) * numOfStock;
+        const profitPercentage = ((profit / (buyPrice * numOfStock))*100).toFixed(2);
         output.style.color="green";
         output.innerText = "Yay!! Your Profit is "+ profit + " and profit Percentage is "+ profitPercentage + "%"+" :)";
 
     }else if(buyPrice>sellPrice){
-        const loss = buyPrice-sellPrice;
-        const lossPercentage = ((loss/buyPrice)*100).toFixed(2);
+        const loss = (buyPrice-sellPrice)*numOfStock;
+        const lossPercentage = ((loss / (buyPrice*numOfStock))*100).toFixed(2);
         output.style.color="red";
         output.innerText= "Whoops!! Your loss is "+loss + " and loss Percentage is "+ lossPercentage + "%"+" :(";
     }else{
         output.style.color="white";
         output.innerText = "No pain No gain, and no gain no pain :)"
     }
+}
 
-    
+function showOutput(){
+
 }
 
 function calculateBtnHandler(){
     const numOfStock = Number(stockNum.value);
-    const buyPrice = Number(initialPrice.value) * numOfStock;
-    const sellPrice = Number(pricesell.value)* numOfStock;
+    const buyPrice = Number(initialPrice.value);
+    const sellPrice = Number(pricesell.value);
     calculateProfitandLoss(buyPrice,sellPrice,numOfStock);
-
-
 }
 calculateBtn.addEventListener("click", calculateBtnHandler);
