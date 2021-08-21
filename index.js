438b331d56e77e401ddb4eb7fc450ef7618f8c49
Shekhar Dhangar <shekhardhangar@yahoo.com>
2021-08-21 16:20:11 +0530

    Made Complete web app
    Build web app

---
+++ index.js
@@ -0,0 +1,37 @@
+const initialPrice = document.querySelector("#buy-price");
+const stockNum = document.querySelector("#stock-num");
+const pricesell = document.querySelector("#sell-price");
+const calculateBtn = document.querySelector(".calculate-btn");
+const output = document.querySelector(".output");
+
+function calculateProfitandLoss(buyPrice,sellPrice,numOfStock){
+
+    if(sellPrice>buyPrice){
+        const profit = sellPrice-buyPrice;
+        const profitPercentage = ((profit/buyPrice)*100).toFixed(2);
+        // return ()
+        output.style.color="green";
+        output.innerText = "Yay!! Your Profit is "+ profit + " and profit Percentage is "+ profitPercentage+ " :)";
+
+    }else if(buyPrice>sellPrice){
+        const loss = buyPrice-sellPrice;
+        const lossPercentage = ((loss/buyPrice)*100).toFixed(2);
+        output.style.color="red";
+        output.innerText= "Whoops!! Your loss is "+loss + " and loss Percentage is "+ lossPercentage+ " :(";
+    }else{
+        output.style.color="white";
+        output.innerText = "No pain No gain, and no gain no pain :)"
+    }
+
+    
+}
+
+function calculateBtnHandler(){
+    const numOfStock = stockNum.value;
+    const buyPrice = (initialPrice.value) * numOfStock ;
+    const sellPrice = (pricesell.value)* numOfStock;
+    calculateProfitandLoss(buyPrice,sellPrice,numOfStock);
+
+
+}
+calculateBtn.addEventListener("click", calculateBtnHandler);
